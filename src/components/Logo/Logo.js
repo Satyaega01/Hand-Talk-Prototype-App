b2f1d3d0cb2c83fa5a5941d91879c810c2440906
Satyaega <108566964+Satyaega01@users.noreply.github.com>
2024-02-20 20:45:43 +0800

    Add files via upload

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,12 @@
+import { div } from "@tensorflow/tfjs";
+import React from "react";
+
+function Logo() {
+  return (
+    <div className="Logo">
+      <img src={require("../../assets/img/Logo + Text.png")} />
+    </div>
+  );
+}
+
+export default Logo;
